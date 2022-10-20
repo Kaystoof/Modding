54eedc66cdcac4cc1028bfb445fab691b69392c4
Kaystoof <KrOzKid@gmail.com>
2022-10-21 00:02:43 +1100

    Test

--- src/main/java/net/krozkid/tutorialmod/TutorialMod.java
+++ src/main/java/net/krozkid/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.krozkid.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.krozkid.tutorialmod.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,6 +24,8 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
